e90428524200e27a20b2b99daa2382cc4d5b1e48
Oldrich Smehlik <oldrich@smehlik.net>
2024-07-14 19:13:39 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# pomodoro
+
+A simple pomodoro timer.
+
+## Usage
+
+Open `index.html` in this folder.
+
+## Screenshots
+
+![](screen1.png)
+![](screen2.png)
+
+## More information
+
+See [Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique) page on Wikipedia.
---
+++ pomodoro.js
@@ -0,0 +1,61 @@
+const pomodoroMinutes = 25;
+
+const txtTimer = document.getElementById("timer");
+const btnStart = document.getElementById("start");
+const btnPause = document.getElementById("pause");
+const btnReset = document.getElementById("reset");
+
+let timerInterval = null;
+let remainingSeconds = pomodoroMinutes * 60;
+
+const getMinutes = () => Math.floor(remainingSeconds / 60);
+const getSeconds = () => remainingSeconds % 60;
+const padZeros = (seconds) => seconds < 10 ? "0" + seconds : seconds;
+
+const updateDisplay = () => {
+    txtTimer.textContent = `${padZeros(getMinutes(remainingSeconds))}:${padZeros(getSeconds(remainingSeconds))}`;
+}
+
+const onTimerZero = () => {
+    clearInterval(timerInterval);
+    btnPause.classList.add("hidden");
+}
+
+const onTimerTick = () => {
+    if (remainingSeconds > 0) {
+        remainingSeconds--;
+    } else {
+        onTimerZero();
+    }
+    updateDisplay();
+}
+
+const onStartClick = (e) => {
+    e.preventDefault();
+    btnStart.classList.add("hidden");
+    btnPause.classList.remove("hidden");
+    btnReset.classList.remove("hidden");
+    timerInterval = setInterval(onTimerTick, 1000);
+}
+
+const onPauseClick = (e) => {
+    e.preventDefault();
+    btnStart.classList.remove("hidden");
+    btnPause.classList.add("hidden");
+    clearInterval(timerInterval);
+}
+
+const onResetClick = (e) => {
+    e.preventDefault();
+    btnStart.classList.remove("hidden");
+    btnPause.classList.add("hidden");
+    btnReset.classList.add("hidden")
+    clearInterval(timerInterval);
+    remainingSeconds = pomodoroMinutes * 60;
+    updateDisplay();
+}
+
+btnStart.addEventListener("click", onStartClick);
+btnPause.addEventListener("click", onPauseClick);
+btnReset.addEventListener("click", onResetClick);
+updateDisplay();
